d1af02d8cceaab5d5e73a75346eb5af980959cdd
NoahSantos <nsanto591@west-mec.org>
2022-08-31 16:52:53 -0700

    Finished
    Got JS code working.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
-let car = prompt("Would you like to learn about the w211 E55 AMG, the w211 E63 AMG, the w212 E63 AMG, the w213 E63 AMG, or the home page?");
+let car = prompt("Would you like to learn about the w211 E55 AMG, the w211 E63 AMG, the w212 E63 AMG, the w213 E63 AMG, or hit cancel for home page?");
 carType(car);
 function carType(type){
     let page = type;
     if(page == "w211 E55 AMG" || page =="W211 E55 AMG" || page == "W211E55AMG" || page == "w211E55AMG" || page == "E55 AMG" || page == "E55AMG" || page == "E55"){
-        location.href("E55 AMG.html");
-    }else if(page == "w211 E63 AMG" ||page == "W211 E63 AMG" || page == "W211E63AMG" || page == "w211E63AMG"){
-        location.href("W211 E63 AMG.html");
-    }else if(page == "w212 E63 AMG" || page == "W212 E63 AMG" || page == "w212E63AMG" || page == "w212" || page == "W212"){
-        location.href("W212 E63 AMG.html");
-    }else if(page == "w213 E63 AMG" || page == "W213 E63 AMG" || page == "W213E63AMG"  || page == "w213E63AMG" || page == "w213" || page == "W213"){
-        location.href("W213 E63 AMG.html");
+        window.location.href = "E55 AMG.html";
+    }else if(page == "w211 E63 AMG" ||page == "W211 E63 AMG" || page == "W211E63AMG" || page == "w211E63AMG" || page == "W211 E63"){
+        window.location.href = "W211 E63 AMG.html";
+    }else if(page == "w212 E63 AMG" || page == "W212 E63 AMG" || page == "w212E63AMG" || page == "w212" || page == "W212" || page == "W212 E63"){
+        window.location.href = "W212 E63 AMG.html";
+    }else if(page == "w213 E63 AMG" || page == "W213 E63 AMG" || page == "W213E63AMG"  || page == "w213E63AMG" || page == "w213" || page == "W213" || page == "W213 E63"){
+        window.location.href = "W213 E63 AMG.html";
     }else if(page.toLowerCase() == "home page" || page.toLowerCase() == "home"){
-        location.href("index.html");
+        window.location.href = "index.html";
     }else{
         let go = prompt("Sorry didn't recognize that car. Would you like to input another?");
         if(go.toLowerCase() == "yes"){
@@ -20,7 +20,7 @@ function carType(type){
             carType(car2);
         }else{
             confirm("Here is a one page summary on the cars.");
-            window.location = "index.html";
+            window.location.href = "index.html";
         }
     }
 }
