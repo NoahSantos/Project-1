ce14069e8ea06ad8307b73fabf1fb06a456b9895
NoahSantos <nsanto591@west-mec.org>
2022-08-31 16:32:40 -0700

    Button and JS
    Added some Js code, but it doesn't fully work right now. Working on fixing it. Added buttons to be able to navigate to different sections.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-//let test = prompt("This is a test. Enter your name.");
+
+let car = prompt("Would you like to learn about the w211 E55 AMG, the w211 E63 AMG, the w212 E63 AMG, the w213 E63 AMG, or the home page?");
+carType(car);
+function carType(type){
+    let page = type;
+    if(page == "w211 E55 AMG" || page =="W211 E55 AMG" || page == "W211E55AMG" || page == "w211E55AMG" || page == "E55 AMG" || page == "E55AMG" || page == "E55"){
+        location.href("E55 AMG.html");
+    }else if(page == "w211 E63 AMG" ||page == "W211 E63 AMG" || page == "W211E63AMG" || page == "w211E63AMG"){
+        location.href("W211 E63 AMG.html");
+    }else if(page == "w212 E63 AMG" || page == "W212 E63 AMG" || page == "w212E63AMG" || page == "w212" || page == "W212"){
+        location.href("W212 E63 AMG.html");
+    }else if(page == "w213 E63 AMG" || page == "W213 E63 AMG" || page == "W213E63AMG"  || page == "w213E63AMG" || page == "w213" || page == "W213"){
+        location.href("W213 E63 AMG.html");
+    }else if(page.toLowerCase() == "home page" || page.toLowerCase() == "home"){
+        location.href("index.html");
+    }else{
+        let go = prompt("Sorry didn't recognize that car. Would you like to input another?");
+        if(go.toLowerCase() == "yes"){
+            let car2 = prompt("Would you like to learn about the w211 E55 AMG, the w211 E63 AMG, the w212 E63 AMG, or the w213 E63 AMG?");
+            carType(car2);
+        }else{
+            confirm("Here is a one page summary on the cars.");
+            window.location = "index.html";
+        }
+    }
+}
